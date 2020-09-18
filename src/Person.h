b20eed0a3efa48705762574eb14d23f0a0c44ddf
ChrisVarsSal <68918398+ChrisVarsSal@users.noreply.github.com>
2020-09-17 20:24:29 -0600

    Se agregó template<typename T> a la clase Person.h

--- src/Person.h
+++ src/Person.h
@@ -11,9 +11,7 @@
 #include <iostream>
 using namespace std;
 
-/**
- * Abstract Class of Person
- */
+template<typename T>
 class Person {
 protected:
    string nom;
@@ -38,4 +36,35 @@ protected:
     virtual double salary()const=0;
     virtual string to_string()const=0;
 };
+template<typename T>
+Person<T>::Person() {}
+
+template<typename T>
+Person<T>::Person(string nom_, string apellido_, int id_){
+    nom=nom_;
+    apellido=apellido_;
+    id=id_;
+}
+
+template<typename T>
+Person<T>::~Person(){ }
+
+template<typename T>
+string Person<T>::get_nom(){return nom;}
+
+template<typename T>
+void Person<T>::set_nom(string x){nom=x;}
+
+template<typename T>
+string Person<T>::get_appellido(){return apellido;}
+
+template<typename T>
+void Person<T>::set_apellido(string x){apellido=x;}
+
+template<typename T>
+int Person<T>::get_ID(){return id;}
+
+template<typename T>
+void Person<T>::set_ID(int x){id=x;}
+
 #endif //LAB02_OOP_PERSON_H

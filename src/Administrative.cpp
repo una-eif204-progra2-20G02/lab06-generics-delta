ffda8f7ab95ed7c00ea18c85bdad6f4a4b975ac4
ChrisVarsSal <68918398+ChrisVarsSal@users.noreply.github.com>
2020-09-17 21:06:35 -0600

    Cambios en la clase Admisnistrative.h

--- src/Administrative.cpp
+++ src/Administrative.cpp
@@ -7,15 +7,35 @@
 
 Administrative::Administrative(double ms):monthly_salary(ms){}
 
-Administrative::Administrative(string nomb,string apel,int id,double salar):Person(nomb,apel,id),monthly_salary(salar){}
+Administrative::Administrative(string nomb,string apel,int id_,double salar)
+:nom(nomb),apellido(apel),id(id_),monthly_salary(salar){}
 
 Administrative::~Administrative(){}
+
+string Administrative::getNombre() {
+    return nom;
+}
+void Administrative::setNombre(string nomb) {
+    nom=nomb;
+}
+string Administrative::getApellido() {
+    return apellido;
+}
+void Administrative::setApellido(string apel) {
+    apellido=apel;
+}
+int Administrative::getId() {
+    return id;
+}
+void Administrative::setId(int id_) {
+    id=id_;
+}
+
+double Administrative::get_monthly_salary() const { return monthly_salary;}
 
 void Administrative::set_monthly_salary(double monthlySalary) {
     Administrative::monthly_salary = monthlySalary;
 }
-
-double Administrative::get_monthly_salary() const { return monthly_salary;}
 
 double Administrative::salary() const{}
 
--- src/Administrative.h
+++ src/Administrative.h
@@ -4,15 +4,30 @@
 
 #ifndef LAB02_OOP_ADMINISTRATIVE_H
 #define LAB02_OOP_ADMINISTRATIVE_H
-#include "Person.h"
+
+#include <iostream>
+#include <sstream>
+using namespace std;
 
 class Administrative{
   double monthly_salary;
+    string nom;
+    string apellido;
+    int id;
 
 public:
   Administrative(double);
   Administrative(string = " ",string = " ", int = 0, double = 0);
   ~Administrative();
+
+  string getNombre();
+  void setNombre(string);
+
+  string getApellido();
+  void setApellido(string);
+
+  int getId();
+  void setId(int);
 
   void set_monthly_salary(double);
   double get_monthly_salary() const;
